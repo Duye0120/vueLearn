41fd03e99ea51f317ad5225f1b252153bc1e12f1
Duye <superdoye0120@outlook.com>
2025-01-13 12:54:16 +0800

    添加progress组件

---
+++ src/views/home/components/ProgressCard.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const weepMap = new Map([
+  ['1', '周一'],
+  ['2', '周二'],
+  ['3', '周三'],
+  ['4', '周四'],
+  ['5', '周五'],
+  ['6', '周六'],
+  ['7', '周日'],
+])
+</script>
+
+<template>
+  <a-card class="p-4 flex flex-col gap-4">
+    <a-typography-text class="font-medium text-base">
+      解决概率
+    </a-typography-text>
+    <div class="grid grid-rows-7 flex-1 mt-4">
+      <div v-for="item in 7" :key="item" class="flex items-center gap-4">
+        <a-progress
+          :key="item"
+          :percent="item * 10"
+          :status="item < 4 ? 'exception' : item < 6 ? 'active' : 'success'" :show-info="false" :size="[300, 20]"
+        />
+        <a-typography-text class="text-sm text-nowrap">
+          {{ weepMap.get(item.toString()) }}
+        </a-typography-text>
+      </div>
+    </div>
+  </a-card>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,20 @@
 <script lang="ts" setup>
+import ProgressCard from './components/ProgressCard.vue'
 import TopCardWithLine from './components/TopCardWithLine.vue'
 import VerticalBar from './components/VerticalBar.vue'
 import { topCardGroupArray } from './data'
 </script>
 
 <template>
-  <div class="w-full flex flex-col gap-5">
-    <div class="h-[148px] w-full grid grid-cols-4 gap-5">
+  <div class="w-full flex flex-col gap-4">
+    <div class="h-[148px] w-full grid grid-cols-4 gap-4">
       <TopCardWithLine v-for="item in topCardGroupArray" :key="item.key" :data="item" />
     </div>
-    <div class="h-[450px] w-full grid !grid-cols-4 gap-5">
+    <div class="h-[450px] w-full grid !grid-cols-4 gap-4">
       <VerticalBar />
+      <ProgressCard />
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
